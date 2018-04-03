2957e94956f4f959c8589132a87f9d89e9b4b83b
Jordan <tamwengjing98@gmail.com>
2018-04-03 10:47:38 +0800

    added D variable

--- p6.cpp
+++ p6.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int main() {
-	int a, b, c;
+	int a, b, c,d ;
 
 	cout << "insert number 1:" << endl;
 	cin >> a;
 	cout << "insert number 2:" << endl;
 	cin >> b;
+	cout <<"insert number 3:" << endl;
+	cin >>c;
+	d = a * b * c;
 
-	c = a * d;
-
-	cout <<"Number 1 * Number 2 = " << c << endl;
+	cout <<"Number 1 * Number 2 * Number 3 = " << d << endl;
 
 	system("pause");
 }
